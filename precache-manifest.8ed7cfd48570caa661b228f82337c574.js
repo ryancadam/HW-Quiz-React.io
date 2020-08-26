f8f1f48b2af02068a9135d753906b4a1712965c3
Ryan Adam <ryancadam88@gmail.com>
2020-08-25 20:35:56 -0500

    Updates

---
+++ precache-manifest.8ed7cfd48570caa661b228f82337c574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85759c54592de4950e37c5c3439b77f",
+    "url": "/HW-Quiz-React.io/index.html"
+  },
+  {
+    "revision": "ed8b245ba0aa229efe1a",
+    "url": "/HW-Quiz-React.io/static/css/main.a3acd1ba.chunk.css"
+  },
+  {
+    "revision": "1ce9f0cce6db5f53cc6d",
+    "url": "/HW-Quiz-React.io/static/js/2.3536e817.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW-Quiz-React.io/static/js/2.3536e817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8b245ba0aa229efe1a",
+    "url": "/HW-Quiz-React.io/static/js/main.bb88a78d.chunk.js"
+  },
+  {
+    "revision": "67848f8d3c7760c934e4",
+    "url": "/HW-Quiz-React.io/static/js/runtime-main.26df46b8.js"
+  }
+]);
